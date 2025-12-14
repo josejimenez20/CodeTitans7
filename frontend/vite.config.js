667099afb84bda7414f238dfda22e01e7ece7ff5
Jose Jimenez <josejimenez2003w@gmail.com>
2025-12-14 09:19:27 -0600

    Eliminar config server https para produccion

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,7 @@
-/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // En producción, Nginx se encarga del puerto y el SSL,
-  // así que no necesitamos la configuración de 'server' aquí para el build.
 })
