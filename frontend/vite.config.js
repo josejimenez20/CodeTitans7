6dd3f2f2ace775d219a0294946793da90ad11997
Jose Jimenez <josejimenez2003w@gmail.com>
2025-12-14 09:10:14 -0600

    Corregir

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,10 @@
 /* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs';
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost+2-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost+2.pem')),
-    },
-    port: 5173,
-  }
+  // En producción, Nginx se encarga del puerto y el SSL,
+  // así que no necesitamos la configuración de 'server' aquí para el build.
 })
